7f56565683f00201aa86b59b28691937b0d1676d
Roice Johnson <rjohn230uncc.edu>
2020-07-17 20:21:03 -0400

    init

---
+++ src/test/java/com/jsware/Seed/SeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsware.Seed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
