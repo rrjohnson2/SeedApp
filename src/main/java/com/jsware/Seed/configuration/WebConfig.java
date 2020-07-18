e74a5ffa30edcb13ad68030e11a1bffd0e7e95a1
Roice Johnson <rjohn230uncc.edu>
2020-07-17 20:42:43 -0400

    spittin truth

---
+++ src/main/java/com/jsware/Seed/configuration/WebConfig.java
@@ -0,0 +1,22 @@
+package com.jsware.Seed.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	private static final Logger log = LoggerFactory.getLogger(WebConfig.class);
+
+	@Override
+	public void addCorsMappings(CorsRegistry reg) {
+		reg.addMapping("/**")// .allowedOrigins("http://twofourviral-env-1.eba-p9mz54re.us-east-2.elasticbeanstalk.com/")
+				.allowedMethods("POST", "GET");
+
+		log.info("Mapping Init");
+	}
+
+}
